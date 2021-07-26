ff1ec488dd5a40d3d561efc7dbaf807d4efafcb6
Lakendria <kendra939@gmail.com>
2021-07-25 20:36:11 -0400

    updated routers

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,49 +1,156 @@
- const express = require('express');
- const router = express.Router();
+const router = require('express').Router();
+const model = require('./auth-model');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {restrict} = require("./auth-middleware")
 
  
 
+router.get("/users",restrict, async (req, res, next) => {
+    try {
+        res.json(await model.find())
+    }
+    catch (err) {
+        next(err)
+    }
+})
+router.post("/login", async (req, res, next) => {
+    try {
+        const { emailId, password } = req.body
+        const user = await model.findByEmailId(emailId)
+        console.log(user.password, "===", password)
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid Credentials"
+            })
+        }
+        //comparing with hashed password 
+        const passwordValid = await bycrypt.compare(password, user.password)
+        console.log({passwordValid})
+        if (!passwordValid) {
+            return res.status(401).json({
+                message: "Invalid password"
+            })
+        }
+        const token = jwt.sign({
+            userID: user.ID,
+            // userRole:user.role,
 
- const Recipe = require('./auth-model');
+        }, "keep it secret,keep it safe")
+        res.cookie("token", token)
+        res.json({
+            message: `Welcome ${user.firstName}!`,
+            data: `${user.ID}`
+        })
+    }
+    catch (err) {
+        next(err)
+    }
+})
 
- const middleware = require('../middleware');
+router.post("/register", async (req, res, next) => {
+    try {
+        const { firstName, lastName, emailId, password, userType } = req.body
+        console.log("emailId", emailId)
+        const users = await model.findByEmailId(emailId)
+        console.log("users router", users)// When i get [] its working 
 
- router.get('/recipe', async(req,res,next)=>{
-    Recipe.getAll()
-    .then(recipes => {
-        res.status(200).json(recipes);
-    })
-    .catch(err =>{
-        res.status(500).json({message:"Error retrieving Recipes", error:err});
-    })
+        if (users) {
+            return res.status(409).json({
+                message: "Your Email Address already registered",
+            })
+        }
+        else {
+
+            const newUser = await model.addUsers({
+                firstName,
+                lastName,
+                emailId,
+                password: await bcrypt.hash(password, 14),//Password harshed 14 times
+                userType
+            })
+            console.log("New user", newUser)
+            res.status(201).json(newUser)
+        }
+
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+ router.post('/:id/addRecipe', async(req,res,next)=>{
+    try {
+        const { recipeName,ingredient,instructions} = req.body
+        const id = req.params.id
+        const newRecipe = await model.addRecipe(req.body, id)
+        console.log("New user", newRecipe)
+        res.status(201).json(newRecipe)
+
+    }
+    catch (err) {
+        next(err)
+    }
  })
 
- router.get('/:id',middleware.checkRecipeId,(req,res,next)=>{
-     res.status(200).json(req.recipe);
- });
+ router.put("/:id/editPotluck", async (req, res, next) => {
+    try {
+        const { recipeName,ingredients, instructions } = req.body
+        const id = req.params.id
+        const idCheck = await model.findById(id)
+        console.log("recipe", idCheck)
+        if (!idCheck) {
+            res.status(409).json({
+                message: "InValid recipe Id"
 
- router.post('/', middleware.checkRecipePayload, async (req,res,next)=>{
-     const recipe = await Recipe.create(req.body);
-     res.status(201).json({message: "Recipe created!"});
- });
+            })
 
- router.put('/:id',middleware.checkRecipePayload, middleware.checkRecipeId, async (req,res,next)=>{
-     const recipe = await Recipe.updateById(req.params.id,req.body);
-     res.status(200).json({message:"Recipe updated!"}, recipe);
- });
+        }
+        const editRecipe = await model.updateRecipe(req.body, id)
+        console.log("Edit", editRecipe)
+        res.status(200).json(editRecipe)
+    }
+    catch (err) {
+        next(err)
+    }
+})
 
- router.delete('/:id', middleware.checkRecipeId, async (req,res,next)=>{
-     await Recipe.deleteById(req.params.id);
-     res.status(204).send("Recipe deleted!");
- });
 
- router.use((err,req,res,next)=>{
-     // eslint-disable-line
-     // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
-     res.status(500).json({
-         message:'something went wrong inside the recipes router', errMessage:err.message,
-     })
-     next()
- })
+router.delete("/:id/deleteRecipe", async (req, res, next) => {
+    const id = req.params.id
+    const deleted = await model.deleteRecipe(id)
+    try {
+        if (deleted) {
+            res.status(200).json({
+                message: "recipe has been deleted"
+            })
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+
+
+
+})
+
+router.get("/logout", async (req, res, next) => {
+	try {
+		// this will delete the session in the database and try to expire the cookie,
+		// though it's ultimately up to the client if they delete the cookie or not.
+        // but it becomes useless to them once the session is deleted server-side.
+        
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
+		next(err)
+	}
+})
+
 
 module.exports = router;
